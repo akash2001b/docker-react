431d60597b474b7c3d23b55eda10963449b5be82
akash Roy Choudhury <arc.ar602@gmail.com>
2021-08-22 22:55:16 +0530

    docker compose setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and h sthere.
         </p>
         <a
           className="App-link"
@@ -23,3 +23,33 @@ function App() {
 }
 
 export default App;
+
+
+// FROM node:alpine
+
+// WORKDIR '/app'
+
+// COPY package.json .
+
+// RUN npm install
+
+// COPY ./ ./
+// # USER root
+
+// CMD ["npm","run","start"]
+
+
+
+
+// FROM node:alpine
+
+// WORKDIR '/app'
+
+// COPY --chown=node:node package.json .
+
+// RUN npm install
+
+// COPY --chown=node:node . .
+// USER node
+
+// CMD ["npm","run","start"]
