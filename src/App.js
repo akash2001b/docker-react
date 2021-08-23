dd55c2caf6ac5d0fbdec7768e3dd213ae4356e87
akash Roy Choudhury <arc.ar602@gmail.com>
2021-08-24 00:49:27 +0530

    react production build with nginx

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and h sthere.
+          Edit <code>src/App.js</code> and there.
         </p>
         <a
           className="App-link"
